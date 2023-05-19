8251a26c4ec5a9e9b0910aedc2cd0bd05d42262e
asakhar <a@sakhar.ru>
2023-05-19 21:07:31 +0300

    optimized mat_dot & benchmarked it

---
+++ bench.c
@@ -0,0 +1,65 @@
+#define NN_IMPLEMENTATION
+#include "nn.h"
+#include <time.h>
+#include <stdio.h>
+
+#define WARM_UP_TIME 3
+#define ITERS 500
+
+void mat_dot_old(Mat dst, Mat a, Mat b);
+
+typedef void (*DotFunc)(Mat, Mat, Mat);
+void bench(Mat dst, Mat a, Mat b, DotFunc func, char const *name);
+
+int main(void)
+{
+  /// setup
+  size_t R = 300;
+  size_t K = 200;
+  size_t C = 400;
+  Mat a = mat_alloc(R, K);
+  Mat b = mat_alloc(K, C);
+  Mat dst = mat_alloc(R, C);
+  mat_rand(a, 0, 1);
+  mat_rand(b, 0, 1);
+
+  bench(dst, a, b, mat_dot_old, "old");
+  bench(dst, a, b, mat_dot, "new");
+}
+
+void bench(Mat dst, Mat a, Mat b, DotFunc func, char const *name)
+{
+  double start = (double)clock() / CLOCKS_PER_SEC;
+  double end = start;
+  printf("Warming up for %d seconds...\n", WARM_UP_TIME);
+  while (end-start < WARM_UP_TIME)
+  {
+    func(dst, a, b);
+    end = (double)clock() / CLOCKS_PER_SEC;
+  }
+  printf("Running bench %s...\n", name);
+  start = (double)clock() / CLOCKS_PER_SEC;
+  for (size_t i = 0; i < ITERS; ++i)
+  {
+    func(dst, a, b);
+  }
+  end = (double)clock() / CLOCKS_PER_SEC;
+  printf("%s solution took: %fs to process\n", name, end - start);
+}
+
+void mat_dot_old(Mat dst, Mat a, Mat b)
+{
+    NN_ASSERT(a.cols == b.rows);
+    size_t n = a.cols;
+    NN_ASSERT(dst.rows == a.rows);
+    NN_ASSERT(dst.cols == b.cols);
+
+    for (size_t i = 0; i < dst.rows; ++i) {
+        for (size_t j = 0; j < dst.cols; ++j) {
+            MAT_AT(dst, i, j) = 0;
+            for (size_t k = 0; k < n; ++k) {
+                MAT_AT(dst, i, j) += MAT_AT(a, i, k) * MAT_AT(b, k, j);
+            }
+        }
+    }
+}
--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ LIBS="`pkg-config --libs raylib` -lm -lglfw -ldl -lpthread"
 clang $CFLAGS -o adder_gen adder_gen.c $LIBS
 clang $CFLAGS -o xor_gen xor_gen.c $LIBS
 clang $CFLAGS -o gym gym.c $LIBS
+clang $CFLAGS -o bench bench.c
