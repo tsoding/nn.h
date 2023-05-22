ce76ab5c781e6ece11c153a0661680fc5a07b24e
rexim <reximkut@gmail.com>
2023-05-22 15:47:01 +0700

    [adder] Introduce STG

--- adder.c
+++ adder.c
@@ -2,11 +2,13 @@
 #define NN_ENABLE_GYM
 #include "nn.h"
 
-#define BITS 4
+#define BITS 5
 
 size_t arch[] = {2*BITS, 4*BITS, BITS + 1};
+size_t epoch = 0;
 size_t max_epoch = 100*1000;
-size_t epochs_per_frame = 103;
+size_t batches_per_frame = 200;
+size_t batch_size = 28;
 float rate = 1.0f;
 bool paused = true;
 
@@ -111,8 +113,7 @@ int main(void)
     SetTextureFilter(font.texture, TEXTURE_FILTER_BILINEAR);
 
     Plot plot = {0};
-
-    size_t epoch = 0;
+    Gym_Batch gb = {0};
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_SPACE)) {
@@ -124,11 +125,13 @@ int main(void)
             plot.count = 0;
         }
 
-        for (size_t i = 0; i < epochs_per_frame && !paused && epoch < max_epoch; ++i) {
-            nn_backprop(nn, g, ti, to);
-            nn_learn(nn, g, rate);
-            epoch += 1;
-            da_append(&plot, nn_cost(nn, ti, to));
+        for (size_t i = 0; i < batches_per_frame && !paused && epoch < max_epoch; ++i) {
+            gym_process_batch(&gb, batch_size, nn, g, t, rate);
+            if (gb.finished) {
+                epoch += 1;
+                da_append(&plot, gb.cost);
+                mat_shuffle_rows(t);
+            }
         }
 
         BeginDrawing();
