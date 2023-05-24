b2517fe5e7c61ca28bf75fbdce880d38e576caae
rexim <reximkut@gmail.com>
2023-05-25 02:05:50 +0700

    [img2nn] add an ability to render the interpolation animation

--- img2nn.c
+++ img2nn.c
@@ -1,9 +1,15 @@
 #include <assert.h>
 #include <stdio.h>
 #include <float.h>
+#include <errno.h>
+#include <string.h>
 
 #include <raylib.h>
 #include <raymath.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "stb_image.h"
 #include "stb_image_write.h"
@@ -12,7 +18,7 @@
 #define NN_ENABLE_GYM
 #include "nn.h"
 
-size_t arch[] = {3, 11, 11, 11, 11, 11, 1};
+size_t arch[] = {3, 11, 11, 9, 1};
 size_t max_epoch = 100*1000;
 size_t batches_per_frame = 200;
 size_t batch_size = 28;
@@ -27,6 +33,124 @@ char *args_shift(int *argc, char ***argv)
     (*argc) -= 1;
     (*argv) += 1;
     return result;
+}
+
+#define out_width 256
+#define out_height 256
+uint32_t out_pixels[out_width*out_height];
+#define FPS 30
+#define STR2(x) #x
+#define STR(x) STR2(x)
+#define READ_END 0
+#define WRITE_END 1
+
+void render_single_out_image(NN nn, float a)
+{
+    for (size_t y = 0; y < out_height; ++y) {
+        for (size_t x = 0; x < out_width; ++x) {
+            MAT_AT(NN_INPUT(nn), 0, 0) = (float)x/(out_width - 1);
+            MAT_AT(NN_INPUT(nn), 0, 1) = (float)y/(out_height - 1);
+            MAT_AT(NN_INPUT(nn), 0, 2) = a;
+            nn_forward(nn);
+            float activation = MAT_AT(NN_OUTPUT(nn), 0, 0);
+            if (activation < 0) activation = 0;
+            if (activation > 1) activation = 1;
+            uint32_t bright = activation*255.f;
+            uint32_t pixel = 0xFF000000|bright|(bright<<8)|(bright<<16);
+            out_pixels[y*out_width + x] = pixel;
+        }
+    }
+}
+
+int render_upscaled_video(NN nn, float duration, const char *out_file_path)
+{
+    int pipefd[2];
+
+    if (pipe(pipefd) < 0) {
+        fprintf(stderr, "ERROR: could not create a pipe: %s\n", strerror(errno));
+        return 1;
+    }
+
+    pid_t child = fork();
+    if (child < 0) {
+        fprintf(stderr, "ERROR: could not fork a child: %s\n", strerror(errno));
+        return 1;
+    }
+
+    if (child == 0) {
+        if (dup2(pipefd[READ_END], STDIN_FILENO) < 0) {
+            fprintf(stderr, "ERROR: could not reopen read end of pipe as stdin: %s\n", strerror(errno));
+            return 1;
+        }
+        close(pipefd[WRITE_END]);
+        
+        int ret = execlp("ffmpeg",
+            "ffmpeg",
+            "-loglevel", "verbose",
+            "-y",
+            "-f", "rawvideo",
+            "-pix_fmt", "rgba",
+            "-s", STR(out_width) "x" STR(out_height),
+            "-r", STR(FPS),
+            "-an",
+            "-i", "-",
+            "-c:v", "libx264",
+            out_file_path,
+            NULL
+        );
+        if (ret < 0) {
+            fprintf(stderr, "ERROR: could not run ffmpeg as a child process: %s\n", strerror(errno));
+            return 1;
+        }
+        assert(0 && "unreachable");
+    }
+
+    close(pipefd[READ_END]);
+
+    typedef struct {
+        float start;
+        float end;
+    } Segment;
+
+    Segment segments[] = {
+        {0, 0},
+        {0, 1},
+        {1, 1},
+        {1, 0},
+    };
+    size_t segments_count = ARRAY_LEN(segments);
+    float segment_length = 1.0f/segments_count;
+
+    size_t frame_count = FPS*duration;
+    for (size_t i = 0; i < frame_count; ++i) {
+        float a = ((float)i)/frame_count;
+        size_t segment_index = floorf(a/segment_length);
+        float segment_progress = a/segment_length - segment_index;
+        if (segment_index > segments_count) segment_index = segment_length - 1;
+        Segment segment = segments[segment_index];
+        float b = segment.start + (segment.end - segment.start)*sqrtf(segment_progress);
+        render_single_out_image(nn, b);
+        write(pipefd[WRITE_END], out_pixels, sizeof(*out_pixels)*out_width*out_height);
+        printf("a = %f, index = %zu, progress = %f, b = %f\n", a, segment_index, segment_progress, b);
+    }
+
+    close(pipefd[WRITE_END]);
+    wait(NULL);
+    printf("Generated %s!\n", out_file_path);
+    return 0;
+}
+
+int render_upscaled_screenshot(NN nn, const char *out_file_path)
+{
+    render_single_out_image(nn, scroll);
+
+    if (!stbi_write_png(out_file_path, out_width, out_height, 4, out_pixels, out_width*sizeof(*out_pixels))) {
+        fprintf(stderr, "ERROR: could not save image %s\n", out_file_path);
+        return 1;
+    }
+
+    printf("Generated %s\n", out_file_path);
+    return 0;
 }
 
 int main(int argc, char **argv)
@@ -144,11 +268,6 @@ int main(int argc, char **argv)
     }
     Texture2D original_texture2 = LoadTextureFromImage(original_image2);
 
-    size_t out_width = 512;
-    size_t out_height = 512;
-    uint8_t *out_pixels = malloc(sizeof(*out_pixels)*out_width*out_height);
-    assert(out_pixels != NULL);
-
     Gym_Batch gb = {0};
     bool rate_dragging = false;
     bool scroll_dragging = false;
@@ -164,24 +283,10 @@ int main(int argc, char **argv)
             plot.count = 0;
         }
         if (IsKeyPressed(KEY_S)) {
-            for (size_t y = 0; y < out_height; ++y) {
-                for (size_t x = 0; x < out_width; ++x) {
-                    MAT_AT(NN_INPUT(nn), 0, 0) = (float)x/(out_width - 1);
-                    MAT_AT(NN_INPUT(nn), 0, 1) = (float)y/(out_height - 1);
-                    MAT_AT(NN_INPUT(nn), 0, 2) = scroll;
-                    nn_forward(nn);
-                    uint8_t pixel = MAT_AT(NN_OUTPUT(nn), 0, 0)*255.f;
-                    out_pixels[y*out_width + x] = pixel;
-                }
-            }
-
-            const char *out_file_path = "upscaled.png";
-            if (!stbi_write_png(out_file_path, out_width, out_height, 1, out_pixels, out_width*sizeof(*out_pixels))) {
-                fprintf(stderr, "ERROR: could not save image %s\n", out_file_path);
-                return 1;
-            }
-
-            printf("Generated %s from %s\n", out_file_path, img1_file_path);
+            render_upscaled_screenshot(nn, "upscaled.png");
+        }
+        if (IsKeyPressed(KEY_X)) {
+            render_upscaled_video(nn, 5, "upscaled.mp4");
         }
 
         for (size_t i = 0; i < batches_per_frame && !paused && epoch < max_epoch; ++i) {
