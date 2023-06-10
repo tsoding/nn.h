c7819243f9fd9cd8b7572b8f15eb139bb48993ba
rexim <reximkut@gmail.com>
2023-06-10 08:25:00 +0700

    Enable building of all the executables

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -xe
 CFLAGS="-O3 -Wall -Wextra -I./thirdparty/ `pkg-config --cflags raylib`"
 LIBS="-lm `pkg-config --libs raylib` -lglfw -ldl -lpthread"
 
-# clang $CFLAGS -o adder adder.c $LIBS
-# clang $CFLAGS -o xor xor.c $LIBS
-# clang $CFLAGS -o img2nn img2nn.c $LIBS
+clang $CFLAGS -o adder adder.c $LIBS
+clang $CFLAGS -o xor xor.c $LIBS
+clang $CFLAGS -o img2nn img2nn.c $LIBS
 clang $CFLAGS -o layout layout.c $LIBS
